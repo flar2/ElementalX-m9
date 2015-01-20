bc81ed881623c269b7e1bac02dfe3f1f52026389
flar2 <asegaert@gmail.com>
2015-01-19 23:24:26 -0500

    ElementalX governor version 2
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -45,6 +45,10 @@
 #define DEFAULT_BUS_P 25
 #define DEFAULT_BUS_DIV (100 / DEFAULT_BUS_P)
 
+#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+int graphics_boost = 4;
+#endif
+
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -315,6 +319,9 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	
 	kgsl_pwrctrl_pwrlevel_change_settings(device, 1, 0);
 
+#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+        graphics_boost = pwr->active_pwrlevel;
+#endif
 	
 	device->pwrscale.freq_change_time = ktime_to_ms(ktime_get());
 }
