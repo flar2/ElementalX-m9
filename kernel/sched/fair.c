dfcba0f7b4e711089a72402238f8ed0941c7fe5e
Joonwoo Park <joonwoop@codeaurora.org>
2015-02-11 16:55:32 -0800

    sched: avoid running idle_balance() on behalf of wrong CPU
    With EA (Energy Awareness), idle_balance() on a CPU runs on behalf of most
    power efficient idle CPU among the CPUs in its sched domain level under the
    condition that the substitute idle CPU should be limited to a CPU which has
    the same capacity with original idle CPU.
    It is found that at present idle_balance() spans all the CPUs in its sched
    domain and run idle balancer on behalf of any CPU within the domain which
    could be all the CPUs in the system which consequently makes idle balancer
    on a performance CPU always runs on behalf of a power efficient idle CPU.
    This would cause for idle performance CPUs to fail to pull tasks from power
    efficient CPUs always when there is only an online performance CPU.
    
    Limit search CPUs to cache sharing CPUs with original idle CPU to ensure to
    run idle balancre on behalf of more power efficient CPU but still has the
    same capacity with original CPU to fix such issue.
    
    Change-Id: I0575290c24f28db011d9353915186e64df7e57fe
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5556,7 +5556,7 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 		return;
 
 	rcu_read_lock();
-	sd = rcu_dereference_check_sched_domain(this_rq->sd);
+	sd = rcu_dereference(per_cpu(sd_llc, this_cpu));
 	if (sd && sched_enable_power_aware) {
 		for_each_cpu(i, sched_domain_span(sd)) {
 			if (i == this_cpu || idle_cpu(i)) {
