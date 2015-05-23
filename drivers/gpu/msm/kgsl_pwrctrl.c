8368764308348bf5a783484de28f59e918f41828
flar2 <asegaert@gmail.com>
2015-05-23 12:09:21 -0400

    cpufreq_elementalx: remove gboost
    We don't need it on this device

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -44,10 +44,6 @@
 
 #define DEFAULT_BUS_P 0
 
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
-int graphics_boost = 4;
-#endif
-
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -277,10 +273,6 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 			pwrlevel->gpu_freq);
 	
 	kgsl_pwrctrl_pwrlevel_change_settings(device, 1, 0);
-
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
-        graphics_boost = pwr->active_pwrlevel;
-#endif
 }
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
