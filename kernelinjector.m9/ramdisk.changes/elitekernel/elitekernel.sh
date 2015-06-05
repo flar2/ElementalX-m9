723f89eb3dda2f1089c25ab987d76255bf89cb17
niker <niker@seznam.cz>
2015-06-05 20:39:17 +0200

    Boot tweaks

--- kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel.sh
+++ kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel.sh
@@ -10,17 +10,22 @@ mount -o remount,ro /system
 sync
 
 # disable KSM and use zcache instead
-echo 0 > /sys/kernel/mm/ksm/run
+#echo 0 > /sys/kernel/mm/ksm/run
 
 # startup time tweaks:
 
 #I/O tweaks
 mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /cache /cache
 mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /data /data
-mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /sd-ext /sd-ext
+mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /sdcard /sdcard
+mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /sdcard2 /sdcard2
 mount -o async,remount,noatime,nodiratime,delalloc,noauto_da_alloc,barrier=0,nobh /devlog /devlog
 echo "2048" > /sys/block/mmcblk0/bdi/read_ahead_kb;
 echo "2048" > /sys/block/mmcblk0/queue/read_ahead_kb;
+echo "2048" > /sys/block/mmcblk1/bdi/read_ahead_kb;
+echo "2048" > /sys/block/mmcblk1/queue/read_ahead_kb;
+echo "2048" > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb;
+echo "2048" > /sys/block/mmcblk0rpmb/queue/read_ahead_kb;
 
 # activate delayed config to override ROM
 /system/xbin/busybox nohup /system/bin/sh /elitekernel/elitekernel_delayed.sh 2>&1 >/dev/null &
--- kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel_delayed.sh
+++ kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel_delayed.sh
@@ -7,36 +7,18 @@ sleep 60
 echo "sio" > /sys/block/mmcblk0/queue/scheduler
 
 # set governors
-echo "smartmax" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "elementalx" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-# set default speeds (cpus activate in order 0-3-2-1)
-echo "1500000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo "51000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
-# set ondemand prefs
-echo "80" > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-echo "15" > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
-echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load
-echo "3000000" > /sys/devices/system/cpu/cpufreq/ondemand/input_boost_duration
-echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-echo "5" > /sys/devices/system/cpu/cpufreq/ondemand/powersave_bias
-echo "5" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-echo "30000" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-echo "10000" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate_min
-echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/touch_poke
-echo "300000" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_bottom_freq
-echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_dynamic
-echo "384000" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_freq
-echo "3" > /sys/devices/system/cpu/cpufreq/ondemand/ui_counter
-echo "20000" > /sys/devices/system/cpu/cpufreq/ondemand/ui_sampling_rate
-echo "66" > /sys/devices/system/cpu/cpufreq/ondemand/ux_boost_threshold
-echo "768000" > /sys/devices/system/cpu/cpufreq/ondemand/ux_freq
-echo "20" > /sys/devices/system/cpu/cpufreq/ondemand/ux_loading
+# set default speeds 0=LP cluster; 4=HP cluster
+echo "1248000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "300000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "1248000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+echo "633600" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
 # set vm tweaks
-sysctl -w vm.min_free_kbytes=8192
+sysctl -w vm.min_free_kbytes=16384
 sysctl -w vm.vfs_cache_pressure=30
-sysctl -w vm.swappiness=20
+sysctl -w vm.swappiness=10
 sysctl -w vm.page-cluster=0
 sysctl -w vm.dirty_expire_centisecs=2400
 sysctl -w vm.dirty_writeback_centisecs=600
@@ -56,17 +38,13 @@ echo "400" > /sys/block/mmcblk0/queue/iosched/sync_write_expire
 echo "240" > /sys/block/mmcblk0/queue/iosched/async_read_expire
 echo "800" > /sys/block/mmcblk0/queue/iosched/async_write_expire
 
-# minfree
-echo "0,1,2,5,7,15" > /sys/module/lowmemorykiller/parameters/adj
-echo "1536,3072,6144,10240,12288,18432" > /sys/module/lowmemorykiller/parameters/minfree
-
 # temporary workaround for stock OTA updater wakelock bugs
 pm disable com.google.android.gms/com.google.android.gms.update.SystemUpdateService\$Receiver
 pm disable com.google.android.gms/com.google.android.gms.update.SystemUpdateService\$SecretCodeReceiver
 pm disable com.google.android.gms/com.google.android.gms.update.SystemUpdateService\$ActiveReceiver
 kill $(pidof com.google.android.gms)
 
-touch /data/local/em_delayed_tweaks
+touch /data/local/ek_delayed_tweaks
 
 # start user init
 # activate delayed config to override Kernel
