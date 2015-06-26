d19926f24bea382d9d28a3db54e33b6a839ea7a3
niker <niker@seznam.cz>
2015-06-26 11:28:48 +0200

    Freq settings

--- kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel_delayed.sh
+++ kernelinjector.m9/ramdisk.changes/elitekernel/elitekernel_delayed.sh
@@ -13,7 +13,7 @@ echo "elementalx" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "960000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo "300000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo "1248000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-echo "633600" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo "384000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
 # force hard freq limits
 echo 960000 > /sys/power/pnpmgr/cluster/little/cpu0/thermal_freq
